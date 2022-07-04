02667149d9a7c4359d96c0b945b343cdf70d3302
eles <eles@MacBook-Air-eles.local>
2022-07-04 20:35:17 +0600

    v1

---
+++ snippets/serializers.py
@@ -0,0 +1,67 @@
+from rest_framework import serializers
+from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+
+
+# class SnippetSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     title = serializers.CharField(required=False, allow_blank=True, max_length=100)
+#     code = serializers.CharField(style={'base_template': 'textarea.html'})
+#     linenos = serializers.BooleanField(required=False)
+#     language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
+#     style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
+
+#     def create(self, validated_data):
+#         """
+#         Create and return a new `Snippet` instance, given the validated data.
+#         """
+#         return Snippet.objects.create(**validated_data)
+
+#     def update(self, instance, validated_data):
+#         """
+#         Update and return an existing `Snippet` instance, given the validated data.
+#         """
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.code = validated_data.get('code', instance.code)
+#         instance.linenos = validated_data.get('linenos', instance.linenos)
+#         instance.language = validated_data.get('language', instance.language)
+#         instance.style = validated_data.get('style', instance.style)
+#         instance.save()
+#         return instance
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+
+
+# from snippets.models import Snippet
+# from snippets.serializers import SnippetSerializer
+# from rest_framework.renderers import JSONRenderer
+# from rest_framework.parsers import JSONParser
+
+# snippet = Snippet(code='foo = "bar"\n')
+# snippet.save()
+
+# snippet = Snippet(code='print("hello, world")\n')
+# snippet.save()
+
+# serializer = SnippetSerializer(snippet)
+# serializer.data
+
+# content = JSONRenderer().render(serializer.data)
+# content
+
+
+# import io
+# stream = io.BytesIO(content)
+# data = JSONParser().parse(stream)
+# serializer = SnippetSerializer(data=data)
+# serializer.is_valid()
+# serializer.validated_data
+# serializer.save()
+# serializer = SnippetSerializer(Snippet.objects.all(), many=True)
+# serializer.data
+
+
+# from snippets.serializers import SnippetSerializer
+# serializer = SnippetSerializer()
+# print(repr(serializer))
